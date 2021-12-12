86aed877810104050ad867eb93237438bf81d7d6
Tenzin <tenthinten@gmail.com>
2021-12-12 16:12:04 +0530

    create topic and message database

--- base/views.py
+++ base/views.py
@@ -10,9 +10,10 @@ from .models import Room
 
 def home(request):
     rooms = Room.objects.all()
-    return render(request,'base/home.html',{'rooms':rooms})
+    context = {'rooms':rooms}
+    return render(request,'base/home.html',context)
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
     context = {'room':room}
-    return render(request,'base/room.html')
+    return render(request,'base/room.html',context)
