7bfe106ce77568b829ddff894fa2a1643eac6348
Tenzin <tenthinten@gmail.com>
2021-12-13 14:59:43 +0530

    have created forms for Room DB

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 # rooms = [
@@ -19,6 +20,6 @@ def room(request,pk):
     return render(request,'base/room.html',context)
 
 def createRoom(request):
-
-    context = {}
+    form = RoomForm()
+    context = {'form':form}
     return render(request, 'base/room_form.html',context)
