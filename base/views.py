d7605fafcc9896fde9be1429b217448094ca8aaa
Tenzin <tenthinten@gmail.com>
2021-12-15 18:21:39 +0530

    User can search with specific words

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.db.models import Q
 from .models import Room,Topic
 from .forms import RoomForm
 
@@ -10,7 +11,13 @@ from .forms import RoomForm
 # ]
 
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__contains=q) |
+        Q(description__contains=q)
+    )
+    
     topics = Topic.objects.all()
     context = {'rooms':rooms,'topics':topics}
     return render(request,'base/home.html',context)
