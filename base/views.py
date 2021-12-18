760024f5cf34f4bd7f2bfb50b60973d633004684
Tenzin <tenthinten@gmail.com>
2021-12-18 18:14:16 +0530

    create user registration form

--- base/views.py
+++ base/views.py
@@ -6,6 +6,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room,Topic
 from .forms import RoomForm
 
@@ -18,8 +19,11 @@ from .forms import RoomForm
 
 def loginPage(request):
     page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+
     if request.method =="POST":
-        username=request.POST.get('username')
+        username=request.POST.get('username').lower()
         password=request.POST.get('password')
 
         try:
@@ -42,8 +46,20 @@ def logoutUser(request):
     return redirect('home')
 
 def registerPage(request):
-    page='register'
-    return render(request,'base/login_register.html')
+    form = UserCreationForm()
+
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username=user.username.lower()
+            user.save()
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'An error occured during registration')
+
+    return render(request,'base/login_register.html',{'form':form})
 
 
 def home(request):
