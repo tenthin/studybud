db896db954e6f0250939d041261311f01ee0f4c0
Tenzin <tenthinten@gmail.com>
2021-12-19 18:00:41 +0530

    how to create activity feed and userprofile

--- base/views.py
+++ base/views.py
@@ -72,24 +72,40 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
-    context = {'rooms':rooms,'topics':topics,'room_count':room_count}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains = q ))
+
+    context = {'rooms':rooms,'topics':topics,
+    'room_count':room_count,'room_messages':room_messages}
     return render(request,'base/home.html',context)
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all()
-    
+    participants = room.participants.all()
     if request.method == 'POST':
         message = Message.objects.create(
             user=request.user,
             room=room,
             body=request.POST.get('body')
         )
+
+        room.participants.add(request.user)
         return redirect('room',pk=room.id)
     
     
-    context = {'room':room,'room_messages':room_messages}
+    context = {'room':room,'room_messages':room_messages,'participants':participants}
     return render(request,'base/room.html',context)
+
+
+def userProfile(request,pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user':user,'rooms':rooms,
+    'room_messages':room_messages,'topics':topics}
+    return render(request,'base/profile.html',context)
+
 
 
 @login_required(login_url='login')
@@ -133,3 +149,15 @@ def deleteRoom(request,pk):
         room.delete()
         return redirect('home')
     return render(request,'base/delete.html',{'obj':room})
+
+@login_required(login_url='login')
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed !')
+
+    if request.method == "POST":
+        message.delete()
+        return redirect('home')
+    return render(request,'base/delete.html',{'obj':message})
