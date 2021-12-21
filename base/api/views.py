238413a59c01f53ebd2b240d076065e80d65ff88
Tenzin <tenthinten@gmail.com>
2021-12-21 15:09:45 +0530

    interface that shows about the api

--- base/api/views.py
+++ base/api/views.py
@@ -1,9 +1,12 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+
+@api_view(['GET'])  #http methods that are allowed to access the views, we can use 'PUT','POST'
 def getRoutes(request):
     routes = [
         'GET/api',
         'GET/api/rooms',
         'GET/api/rooms/:id'
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
